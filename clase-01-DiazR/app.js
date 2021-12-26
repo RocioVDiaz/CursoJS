c867f0cbafa6ddf109da9484de26c17391d43c3c
RocioVDiaz <93826975+RocioVDiaz@users.noreply.github.com>
2021-12-25 23:01:54 -0300

    Add files via upload

---
+++ clase-01-DiazR/app.js
@@ -0,0 +1,10 @@
+let entrada = prompt("Ingresa tu nombre");
+let pregunta = prompt ("Ingresa tu año de nacimiento");
+let mensaje = "Hola";
+let edad = 2022 - pregunta;
+let resultado = mensaje +" "+ entrada + " cumplis "+ edad + " años";
+alert(resultado);
+
+
+
+
